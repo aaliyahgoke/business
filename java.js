cc2fab89b1fdd154a355f4f2558ff7b6b0bac1fa
aaliyahgoke <aaliyahadegoke55@gmail.com>
2025-03-26 14:38:48 +0100

    update

---
+++ java.js
@@ -0,0 +1,15 @@
+document.queryselectorAll('.read-more-btn').forEach(button=>{
+    button.addEventListener('click',()=>{
+        const contentBox =button.parentElement;
+        const fullText =
+        contentBox.queryselector('full-text');
+
+        if(fullText.style.display===
+     "none" || fullText.style.display ==="") {
+            fulllText.stlye.display =
+     
+    "block" 
+
+        
+    })
+})

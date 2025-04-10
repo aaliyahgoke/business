637d8275e63458a713a18f288a1421540ad2f580
aaliyahgoke <aaliyahadegoke55@gmail.com>
2025-04-10 16:02:55 +0100

    update

--- java.js
+++ java.js
@@ -1,7 +1,5 @@
-//get the div for the ham menu
-const HamburgerMenu = document.getElementById("hamB");
-//event listern to notify when is clicked
-HamburgerMenu.onclick= function togglemenu(){
-    const nav = document.getElementById("navlinks");
-    nav.classList.toggle("show");
+function toggleMenu() {
+    const nav = document.getElementById("hiddenNav");
+    nav.classList.toggle("show-nav");
 }
+event.preventDefault();

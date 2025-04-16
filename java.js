dcd1e1b05be16f61c2922d4caa7d2ad69e6dfd8b
aaliyahgoke <aaliyahadegoke55@gmail.com>
2025-04-16 15:55:07 +0100

    update

--- java.js
+++ java.js
@@ -1,7 +1,4 @@
-function togglemenu() {
-    const hiddenNav = document.getElementById('hiddenNav');
-    hiddenNav.classList.toggle('show-nav');
-}
+
 const navbar = document.querySelector('.navbar');
 const scrollLeftBtn = document.querySelector('.scroll-left');
 const scrollRightBtn = document.querySelector('.scroll-right');
@@ -12,4 +9,15 @@ scrollLeftBtn.addEventListener('click', () => {
 
 scrollRightBtn.addEventListener('click', () => {
   navbar.scrollBy({ left: 200, behavior: 'smooth' });
+});
+function toggleMenu(icon) {
+  icon.classList.toggle("change");
+  document.querySelector(".navbar hidden").classList.toggle("show");
+  document.querySelector(".navbar hidden").classList.toggle("hidden");
+}
+document.querySelectorAll('.item').forEach(item => {
+  item.addEventListener('click', () => {
+    const itemName = item.querySelector('span').textContent;
+    alert(`You selected: ${itemName}`);
+  });
 });
